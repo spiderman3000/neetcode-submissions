c53b007b25b04e142c6da0ad8c44cd83c416017b
spiderman3000 <milesmorales13000@gmail.com>
2026-04-20 01:35:18 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    bool isPalindrome(string s){
+        int l = 0, r = s.size() - 1;
+        while(l < r){
+            if(s[l++] == s[r--]) continue;
+            return false;
+        }
+        return true;
+    }
+public:
+    bool validPalindrome(string s) {
+        int l = 0, r = s.size() - 1;
+        while(l < r){
+            if(s[l] == s[r]) {
+                l++;
+                r--;
+                continue;
+            }
+            return isPalindrome(s.substr(0, l)+s.substr(l+1, s.size() - l -1)) || isPalindrome(s.substr(0, r)+s.substr(r+1, s.size() - r -1));
+        }
+        return true;
+    }
+};
