3b14edcad60c1f8fcd64327fb164b8500c4c12e1
spiderman3000 <milesmorales13000@gmail.com>
2026-05-03 14:02:08 +0530

    Add: find-the-town-judge - submission-0

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+       vector<int> inDegree(n+1, 0), outDegree(n+1,0);
+       for(int i=0;i<trust.size();i++){
+        inDegree[trust[i][1]]++;
+        outDegree[trust[i][0]]++;
+       }
+       for(int i=1;i<=n;i++){
+        if(inDegree[i] == n-1 && outDegree[i] == 0) return i;
+       }
+       return -1;
+    }
+};
